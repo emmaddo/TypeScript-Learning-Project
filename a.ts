469041cf5c0a7d54ab00ed609403c0b231898c8a
emmaddo <ermsapp@gmail.com>
2024-06-02 11:31:19 +0100

    Stage 3: Refactored::Assinged the method in the class to a variable using let Addition = inst.addition(5, 45); since methods inside a class cannot be directly assinged to a variable this way. Just for learning. Smile

--- a.ts
+++ a.ts
@@ -1,22 +1,21 @@
 class doIt{
     
-    addition(a: number, b: number): string{
-let result = a + b;
-if(result != 50){
-    throw new Error("Am sorry Boss, there is a problem");
-}
-return `The result is: ${result}`;
+     addition = function addition(a: number, b: number){
+let cond: number = a + b;
+// return result != 50 ? "It is not 50" : "It is 50";
+let result: string = cond != 50 ? "Not 50" : "It is 50";
+return result;
 }
 
 add = (a: number, b: number): number => (a + b);
 }
 
 const inst = new doIt();
+let dAddition = inst.addition(5, 45);
 try{
-    console.log(inst.addition(5, 45));
-    console.log(`The result is: ${inst.add(1,1)}`);
+    console.log(`The first Addition is: ${dAddition}`);
+    console.log(`The second Addition is: ${inst.add(1,2)}`);
 }
 catch(error: any){
     console.error("There is an error:", error);
 }
-
